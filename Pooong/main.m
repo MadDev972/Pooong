5a4398fe46f3e467cb300bb7bdf80d8c34b5bc39
Sofia972 <sofia_dev@live.com>
2013-02-26 17:30:00 +0100

    Initial Commit

---
+++ Pooong/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Pooong
+//
+//  Created by Sofia Maria Natacha Chevrolat on 2/26/13.
+//  Copyright MadDev 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
